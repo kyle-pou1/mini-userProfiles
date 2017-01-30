b4b1fe7f31763fff13b2a9f55b9423df2611da18
Macbook <kyle.poul@gmail.com>
2017-01-30 16:25:29 -0700

    promise

--- controller.js
+++ controller.js
@@ -1,13 +1,8 @@
-angular.module('userProfiles').controller('MainController', function($scope, mainService){
+angular.module('userProfiles').controller('mainController', function($scope, mainService){
 
-    $scope.thisAppIsBroken = "This angular app is working";
-    $scope.getUsers = function() {
-      $scope.users = mainService.getUsers();
-    }
-    $scope.getUsers();
+  $scope.getUsers = function() {
+  $scope.users = mainService.getUsers();
+}
 
-    $scope.toggleFavorite = mainService.toggleFavorite;
-
-
-
-})
+$scope.getUsers();
+});
